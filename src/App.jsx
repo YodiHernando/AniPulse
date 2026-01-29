cd926bc8aa71283705910d06db46f6adeb8f1791
YodiHernando <nasyadevika73865@gmai.com>
2026-01-29 16:45:25 +0700

    Add Vercel Speed Insights

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Layout from './components/layout/Layout';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 // Pages
 import Home from './pages/Home';
@@ -25,6 +26,7 @@ function App() {
         <QueryClientProvider client={queryClient}>
             <Router>
                 <ScrollToTop />
+                <SpeedInsights />
                 <Layout>
                     <Routes>
                         <Route path="/" element={<Home />} />
